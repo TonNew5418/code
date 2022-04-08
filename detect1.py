50e634d6bdc95f8740127f8d77dfb70632e68c3f
TonNew5418 <90179098+TonNew5418@users.noreply.github.com>
2022-04-08 18:32:16 +0800

    Update detect1.py

--- detect1.py
+++ detect1.py
@@ -1,4 +1,3 @@
-import os
 from math import ceil
 
 import cv2
@@ -9,16 +8,10 @@ def detect(readimg):
     readimg = cv2.cvtColor(readimg, cv2.COLOR_BGR2GRAY)
     readimg = cv2.medianBlur(readimg, 3)
     readimg = cv2.Canny(readimg, 200, 100)
-    cv2.imshow('image', readimg)
 
     circles = cv2.HoughCircles(readimg, cv2.HOUGH_GRADIENT, 1, 50, param1=50, param2=20, minRadius=80, maxRadius=130)
-    cv2.waitKey()
+
     if circles is not None:
         return ceil(circles[0, 0, 2]), ceil(circles[0, 0, 0]), ceil(circles[0, 0, 1])
     else:
         return 0, 0, 0
-
-if __name__ == '__main__':
-    img_path = os.path.join('images', '2.png')
-    img = cv2.imread(img_path)
-    detect(img)
