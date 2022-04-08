66c0928be0603ea8b201ee4db5374b8eab45be5e
TonNew5418 <90179098+TonNew5418@users.noreply.github.com>
2022-04-08 18:25:40 +0800

    Update detect1.py

--- detect1.py
+++ detect1.py
@@ -10,9 +10,9 @@ def detect(readimg):
     readimg = cv2.medianBlur(readimg, 3)
     readimg = cv2.Canny(readimg, 200, 100)
     cv2.imshow('image', readimg)
-    cv2.waitKey()
-    circles = cv2.HoughCircles(readimg, cv2.HOUGH_GRADIENT, 1, 50, param1=50, param2=20, minRadius=80, maxRadius=100)
 
+    circles = cv2.HoughCircles(readimg, cv2.HOUGH_GRADIENT, 1, 50, param1=50, param2=20, minRadius=80, maxRadius=130)
+    cv2.waitKey()
     if circles is not None:
         return ceil(circles[0, 0, 2]), ceil(circles[0, 0, 0]), ceil(circles[0, 0, 1])
     else:
