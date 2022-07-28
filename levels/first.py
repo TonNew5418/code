8479e28d34f7b3d3db5c4c81b5537ea3d1f68a7c
TonNew5418 <2624490035@qq.com>
2022-07-28 14:24:16 +0800

    调整了一下第一关代码（之前测试时第一关通过，可以不修改直接使用）

--- levels/first.py
+++ levels/first.py
@@ -1,12 +1,13 @@
 import cv2
 import numpy as np
 import time
+
 from levels.necessary_functions import getAreaMaxContour
 import RobotControl.Serial_Servo_Running as SSR
+from running_parameters import color_dict
 
 
-def start_door(width, height, org_img, debug, color_dict):
-    #     SSR.serial_setServo(20, 450, 500)
+def main(org_img, width=320, height=240, debug=True):
     SSR.serial_setServo(20, 450, 500)
     time.sleep(1)
     door_count = 0
@@ -26,7 +27,6 @@ def start_door(width, height, org_img, debug, color_dict):
         frame_door = cv2.add(frame_door1, frame_door2)
         opened = cv2.morphologyEx(frame_door, cv2.MORPH_OPEN, np.ones((3, 3), np.uint8))  # 开运算 去噪点
         closed = cv2.morphologyEx(opened, cv2.MORPH_CLOSE, np.ones((3, 3), np.uint8))  # 闭运算 封闭连接
-        # (image, contours, hierarchy) = cv2.findContours(closed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)  # 找出轮廓
         _, contours, hierarchy = cv2.findContours(closed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)  # 找出轮廓
         areaMaxContour, area_max = getAreaMaxContour(contours, 25)  # 找出最大轮廓
         percent = round(100 * area_max / (width * height), 2)  # 最大轮廓的百分比
@@ -37,8 +37,6 @@ def start_door(width, height, org_img, debug, color_dict):
             if debug:
                 cv2.drawContours(org_img_copy, [box], 0, (153, 200, 0), 2)  # 将最小外接矩形画在图上
         if debug:
-            #             cv2.imshow('orginal frame', org_img)
-            #             cv2.imshow('closed', closed)  # 显示图像
             cv2.putText(org_img_copy, 'area: ' + str(percent) + '%', (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 0.7,
                         (0, 0, 255), 2)
             t2 = cv2.getTickCount()
@@ -46,9 +44,6 @@ def start_door(width, height, org_img, debug, color_dict):
             fps = 1.0 / time_r
             cv2.putText(org_img_copy, "fps:" + str(int(fps)), (30, 200), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (0, 0,
                                                                                                           255), 2)
-            # cv2.moveWindow('orgFrame', img_center_x, 100)  # 显示框位置
-        # cv2.imshow('org_img_copy', org_img_copy)  # 显示图像
-        # cv2.waitKey(3)
 
         # 根据比例得到是否前进的信息
         if percent > 20:
@@ -71,6 +66,3 @@ def start_door(width, height, org_img, debug, color_dict):
         else:
             pass
         print("door_count = ", door_count, " no_door_count = ", no_door_count)
-
-#         cv2.destroyAllWindows()
-#         time.sleep(0.5)
