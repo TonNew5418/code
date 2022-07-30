83dc2b45295f7dc60328dfac04508dc5a4b4eb50
TonNew5418 <2624490035@qq.com>
2022-07-30 14:21:45 +0800

    添加了levels的init

---
+++ levels/__init__.py
@@ -0,0 +1,5 @@
+from RobotControl import Serial_Servo_Running
+from RobotControl import SerialServoCmd
+from RobotControl import config_serial_servo
+
+__all__ = ["door", "first", "necessary_functions", "sec_trap"]
