73440d5d8f60b8ac87ad35af286121dc6951d503
MarkZhang <1339397817@qq.com>
2022-08-20 16:35:33 +0800

    20220820

--- levels/stairs.py
+++ levels/stairs.py
@@ -1,14 +1,57 @@
 import cv2
+import threading
 import numpy as np
 import time
 
+import main
+import running_parameters
 from levels.necessary_functions import find_center
 import RobotControl.Serial_Servo_Running as SSR
 from running_parameters import color_dict
 
-def main(org_img):
+cap = cv2.VideoCapture(-1)
+org_img = None
+ret = False
+
+map1 = running_parameters.map1
+map2 = running_parameters.map2
+
+
+# debug = main.debug
+# isstop = main.istop
+
+
+def get_img():
+    global cap, org_img, ret
+    while True:
+        if cap.isOpened():
+            ret, org_img_fish = cap.read()
+            org_img = cv2.remap(org_img_fish.copy(), map1, map2, interpolation=cv2.INTER_LINEAR,
+                                borderMode=cv2.BORDER_CONSTANT)
+
+            if ret:
+                cv2.imshow('original frame', org_img)
+                time.sleep(0.6)
+                cv2.waitKey(3)
+
+            else:
+                time.sleep(0.01)
+        else:
+            time.sleep(0.01)
+
+
+def main():
     print("stairs")
     time_start_adjust = time.time()
+
+    th1 = threading.Thread(target=get_img)
+    th1.setDaemon(True)  # 设置为后台线程，这里默认是False，设置为True之后则主线程不用等待子线程
+    th1.start()
+
+    while org_img is None:
+        time.sleep(0.5)
+        print('delay 0.5s')
+
     while True:
         find_center(org_img)
         cv2.waitKey(3)
@@ -64,7 +107,6 @@ def main(org_img):
 
     SSR.change_action_value("star_down_single_final", 1)
     time.sleep(10)
-    
 
     SSR.change_action_value("go_middle_stair", 1)
     time.sleep(3)
@@ -75,7 +117,7 @@ def main(org_img):
     # 下坡
     SSR.change_action_value("go_down", 10)
     time.sleep(15)
-    
+
     #
     SSR.change_action_value("go_middle_stair", 1)
     time.sleep(3)
